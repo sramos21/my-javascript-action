2ad1f213d769916eea1579ee319137a12a5e3fc3
Sergio <sramos@zivra.com>
2022-03-20 21:02:50 -0600

    my github action

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core')
+const github = require('@sctions/github')
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGeet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    //Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    } catch (error) {
+      core.setFailed(error.message);
+      }
